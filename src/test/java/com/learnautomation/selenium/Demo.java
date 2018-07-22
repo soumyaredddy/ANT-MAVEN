cfab913f4a011116ee76173ab08dd79d2d408b05
soumya <soumyareddy.671@gmail.com>
2018-07-21 21:24:07 -0500

    practiced by mukesh

---
+++ src/test/java/com/learnautomation/selenium/Demo.java
@@ -0,0 +1,12 @@
+package com.learnautomation.selenium;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void method1() {
+		System.out.println("Demo for mavrn class");
+	}
+	
+}
